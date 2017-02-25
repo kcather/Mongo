62d0224bda1ea031329da99abeef813c2a58d7e0
vagrant <vagrant@debian-8.7-amd64>
2017-02-25 20:38:27 +0000

    foodcritic corrections

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,5 @@ license          'All rights reserved'
 description      'Installs/Configures mongo.rb'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-
+issues_url 'https://github.com/kcather/Mongo/tree/master' if respond_to?(:issues_url)
+source_url 'https://github.com/kcather/Mongo/tree/master' if respond_to?(:source_url)
